39a8f412cc8c5650ad1b5688e83741a8fd791413
yysaki <yysaki@mba>
2013-09-03 23:08:36 +0900

    bit calc completed

--- sort.c
+++ sort.c
@@ -4,13 +4,13 @@
 #define SWAP(x, y) x = x + y; y = x - y; x = x - y;
 #define ORDER(x, y) if(x > y){ SWAP(x, y); }
 
-#define NIL -1
-#define ISNIL(num) (num > 0) ? NIL : num 
-
+#define J(i) (((i>>8)-1) / ((i&255)+1))
+#define K(i) (((i>>8)-1) % ((i&255)+1))
 
 int sort(int* a, long i){
-  for(i = (i*i*256) + (i-1); i >= 256; i--){
+  for(i = (i*i*256) + (i-1); i >= 256; i = i - 256){
     ORDER(a[J(i)], a[K(i)]);
+    printf("J=%ld, K=%ld, masked=%ld\n", J(i), K(i), i & 255);
   }
 }
 
