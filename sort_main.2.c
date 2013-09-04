e2160fe0afe9cd820efe05366ab123e1fa11f3c2
yysaki <yysaki@mba>
2013-09-05 01:40:23 +0900

    sort.2.cpp

--- sort_main.2.c
+++ sort_main.2.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 
 #define SWAP(x, y) x = x + y; y = x - y; x = x - y;
-#define ORDER(x, y) if(x > y){ SWAP(x, y); }
 
-#define J(i) (((i>>8)-1) / ((i&255)+1))
-#define K(i) (((i>>8)-1) % ((i&255)+1))
+#define J(i) ((i >> 8 ) - 1) / ((i & 255) + 1)
+#define K(i) ((i >> 8 ) - 1) % ((i & 255) + 1)
 
 int sort(int* a, int i){
-  for(i = (i*i*256) + (i-1); i >= 256; i = i - 256){
-    ORDER(a[J(i)], a[K(i)]);
-    printf("J=%d, K=%d, masked=%d\n", J(i), K(i), i & 255);
+  for(i = ((i * i) << 8) + i - 1; i >= (1 << 8); i = i - (1 << 8)){
+    if(a[J(i)] > a[K(i)]){ SWAP(a[J(i)], a[K(i)]); }
   }
 }
 
